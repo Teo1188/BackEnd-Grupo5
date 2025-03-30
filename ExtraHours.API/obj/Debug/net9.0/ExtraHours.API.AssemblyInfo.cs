b418867db373e4545e65f289920bfbb9b3d1677d
Tadeo Gaviria <tadeogaviria@gmail.com>
2025-03-30 09:05:00 -0500

    push carpeta principal Ref-g1-52min

--- ExtraHours.API/obj/Debug/net9.0/ExtraHours.API.AssemblyInfo.cs
+++ ExtraHours.API/obj/Debug/net9.0/ExtraHours.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExtraHours.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7e40621c1805663698738a2db6384060336258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd10bdd083e06ba045b9ffea8d13061d7224389")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExtraHours.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExtraHours.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ExtraHours.CORE/obj/Debug/net9.0/ExtraHours.CORE.AssemblyInfo.cs
+++ ExtraHours.CORE/obj/Debug/net9.0/ExtraHours.CORE.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ExtraHours.CORE")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ExtraHours.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7e40621c1805663698738a2db6384060336258")]
-[assembly: System.Reflection.AssemblyProductAttribute("ExtraHours.CORE")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ExtraHours.CORE")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd10bdd083e06ba045b9ffea8d13061d7224389")]
+[assembly: System.Reflection.AssemblyProductAttribute("ExtraHours.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ExtraHours.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
