40b88838e62a0be4b48f5cc5568f27a3043d29e0
Tadeo Gaviria <tadeogaviria@gmail.com>
2025-04-03 14:43:32 -0500

    ajustes tablas y bd

--- ExtraHours.CORE/Services/IUserService.cs
+++ ExtraHours.CORE/Services/IUserService.cs
@@ -1,11 +1,10 @@
 using ExtraHours.Core.Models;
-
+ 
 namespace ExtraHours.Core.Services
-
 {
     public interface IUserService
     {
-        Task<string?> Authenticate(string email, string password);
+        Task<(User? user, string? token)> Authenticate(string email, string password);
         Task<User> Register(User user);
         Task<IEnumerable<User>> GetUsers();
         Task<User?> GetUserById(int id);
